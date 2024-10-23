d0b8d5d3251e4be46ad97839d6441d0bd5ad1fc1
Sandip Ghimire <sandipghimire254@gmail.com>
2024-10-23 16:08:32 +0545

    Datatype Learning

---
+++ JS/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("Hello! This is me Sandip") // we are using node js, not browser
+
+console.log(3+3); 
+console.log("Sandip") // code readability should be high
+
+let name = "name"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigInt 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null) // object
