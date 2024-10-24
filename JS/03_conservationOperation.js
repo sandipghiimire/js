5a9270b45d0c4c53f59f9203d3eab9002bfdd33b
Sandip Ghimire <sandipghimire254@gmail.com>
2024-10-24 12:10:24 +0545

    conservationOperation

---
+++ JS/03_conservationOperation.js
@@ -0,0 +1,26 @@
+let score =true
+
+console.log(typeof score)
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33asd" => NaN
+//  true =>1; false=>0;
+
+let isLoggedIn = ""
+let booleanIsLoggesIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggesIn);
+
+// 1 = > true; 0 => false;
+// "" => false
+// "sandip" => ttrue
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+cons
