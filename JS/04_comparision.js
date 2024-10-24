593d547d1034e7d0c0063d64ddfdb725df9b1398
Sandip Ghimire <sandipghimire254@gmail.com>
2024-10-24 12:13:28 +0545

    comparision operation

--- JS/04_comparision.js
+++ JS/04_comparision.js
@@ -1,1 +1,13 @@
 console.log(2>4);
+console.log(2+4);
+console.log(2-4);
+console.log(2/4);
+console.log(2%4);
+console.log(2**4);
+
+
+console.log("2">1);
+console.log("2">=1);
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
