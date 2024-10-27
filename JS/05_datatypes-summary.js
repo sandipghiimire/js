74d9dd49a53be207d8f846c2c9dfb719ba88850b
Sandip <sandipghimire254@gmail.com>
2024-10-27 19:33:57 +0545

    Updated

---
+++ JS/05_datatypes-summary.js
@@ -0,0 +1,12 @@
+// Stack(Primitivr), Heap(Non-Primitive)
+
+let myYoutubeName = "Snadip"
+let anotheName = myYoutubeName
+anotheName = "Sandip II"
+console.log(myYoutubeName);
+console.log(anotheName);
+
+
+let user = {
+    
+}
